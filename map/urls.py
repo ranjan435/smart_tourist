009e84927d022e54436d44bf66622101fc23dd39
ranjan <074bex435.ranjan@pcampus.edu.np>
2021-09-03 22:14:26 +0545

    added search functionality, detail view, and recommend view

--- map/urls.py
+++ map/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
-from .views import index
+from .views import index,recommend,detail,search_recommend
 
 urlpatterns = [
     path('',index,name='home'),
+    path('recommend',recommend,name='recommend'),
+    path('place/<int:place_id>', detail, name='detail'),
+    path('search', search_recommend, name='search'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Place 
+from .forms import SearchForm
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
     return render(request, 'map/hy.html',{
         'places': Place.objects.all()
     })
+
+def recommend(request):
+    return render(request, 'map/recommend.html',{
+        'places': Place.objects.all()
+    })
+
+def detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    context = {
+        'place': place
+    }
+    return render(request, 'map/detail.html', context)
+
+def search_recommend(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = request.POST['search']
+            place = Place.objects.filter(Q(name__icontains=query) | Q(description__icontains=query)).distinct()
+            return render(request, 'map/recommend.html',{
+                'places': place
+            })
+    else:
+        form = SearchForm()
+    return render(request, 'map/search.html', {'form': form})
+        
