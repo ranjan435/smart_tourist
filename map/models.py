8523d2687d58be89c3ba709901ffed6e162df478
ranjan <074bex435.ranjan@pcampus.edu.np>
2021-09-04 11:46:46 +0545

    added default image field for practise

--- map/models.py
+++ map/models.py
@@ -7,7 +7,7 @@ from django.contrib.gis.geos import Point
 class Place(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    photo = models.ImageField(blank=True, upload_to='place')
+    photo = models.ImageField(blank=True, upload_to='place',default='media/place/kathmandu_durbar_square.png')
     location= LocationField(based_fields=['pulchowk campus'],zoom=7,default=Point(85.3178166,27.6828417))
 
     def __str__(self):
