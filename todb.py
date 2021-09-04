aa3075e6fb1435f513892d4a05e3c32dd3728617
ranjan <074bex435.ranjan@pcampus.edu.np>
2021-09-04 22:15:43 +0545

    added latitude and longitude in place model from csv

--- todb.py
+++ todb.py
@@ -5,6 +5,7 @@ import django
 django.setup()
 
 from map.models import Place
+from django.contrib.gis.geos import Point
 import uuid
 from django.core.files.base import ContentFile
 import base64
@@ -17,9 +18,9 @@ def csv_to_image(data):
     data = ContentFile(base64.b64decode(imgstr),name=str(uuid.uuid4())+'.'+ext)
     return data
 
-with open('destinations_of_nepal_updated_with_image_url.csv') as csvfile:
+with open('data_all.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        place = Place.objects.create(name=row['title'],description=row['genre'],photo=csv_to_image(row['img_url']))
+        place = Place.objects.create(name=row['title'],description=row['genre'],photo=csv_to_image(row['img_url']),location=Point(float(row['longitude']),float(row['latitude'])))
         place.save()
 
